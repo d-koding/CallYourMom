91f34a05b56590210127b068bb2757ca83783333
Dylan O’Connor <dylanoconnor@Dylans-MacBook-Pro.local>
2024-04-13 12:53:23 -0700

    updated frontend

--- services/client/my-react-app/src/App.js
+++ services/client/my-react-app/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import TolerancePage from './Tolerance';
+import LoginPage from './Login';
+import HomePage from './HomePage';
+import NotFoundPage from './NotFoundPage';
+import ContactsPage from './ContactsPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<LoginPage />} /> {/* Default route */}
+          <Route path="/Tolerance" element={<TolerancePage />} />
+          <Route path="/Contacts" element={<ContactsPage />} />
+          <Route path="/HomePage" element={<HomePage />} />
+          <Route path="*" element={<NotFoundPage />} /> {/* This route will be matched for any other route */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
