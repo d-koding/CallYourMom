564582ce2e038e0febeca28924a48d6a71c734b1
Dylan O’Connor <dylanoconnor@Dylans-MacBook-Pro.local>
2024-04-13 14:12:52 -0700

    gitignore and venv

--- services/client/my-react-app/src/App.js
+++ services/client/my-react-app/src/App.js
@@ -1,17 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './Login';
 import TolerancePage from './Tolerance';
 import NotFoundPage from './NotFoundPage';
 
 function App() {
+  const [data, setData] = useState([]); // Start with an empty array
+
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then(res => {
+        if (res.ok) {
+          return res.json(); // Parse JSON only if response is ok
+        }
+        throw new Error('Network response was not ok.'); // Throw error on bad response
+      })
+      .then(data => {
+        setData(data.users); // Update state with fetched users
+        console.log(data.users);
+      })
+      .catch(error => console.error('There was a problem with the fetch operation:', error));
+  }, []); // Empty dependency array means this effect will only run once after the initial render
+
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route exact path="/" element={<LoginPage />} /> {/* Default route */}
-          <Route exact path="/Tolerance" element={<TolerancePage />} /> {/* Initial Logins Route Here */}
-          <Route path="*" element={<NotFoundPage />} /> {/* This route will be matched for any other route */}
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/tolerance" element={<TolerancePage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
     </Router>
@@ -19,4 +36,3 @@ function App() {
 }
 
 export default App;
-
--- services/flask-server/server.py
+++ services/flask-server/server.py
@@ -1,12 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-# Members API Route
 @app.route("/users")
 def members():
     return {"users": ["Member1", "Member2", "Member3"]}
-
 
 if __name__ == "__main__":
     app.run(debug=True)
