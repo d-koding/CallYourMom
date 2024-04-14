8f9bfe056edcad7a705ce5aced20f5f5fb4cf7a9
Dylan O’Connor <dylanoconnor@Dylans-MacBook-Pro.local>
2024-04-13 21:23:12 -0700

    edits

--- services/client/my-react-app/src/startDrinking.js
+++ services/client/my-react-app/src/startDrinking.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './startDrinking.css'; // Make sure this is the correct path to your CSS file
+import logo from './logo.png';
 
 function StartDrinking() {
   const navigate = useNavigate();
@@ -14,7 +15,7 @@ function StartDrinking() {
     <div className="start-page-container">
       <div className="beverage-icon-container">
         {/* Replace "beverage-icon.png" with the actual path to your image */}
-        <img src="logo.png" alt="Beverage" className="beverage-icon" />
+        <img src={logo} alt="Beverage" className="beverage-icon" />
       </div>
       <button onClick={handleStart} className="start-button">Start Drinking</button>
       <div className="navigation-bar">
